cd867b846eebda98ce922de47e6cb9d48816707f
Duncan Campbell <duncansandersoncampbell@gmail.com>
2023-06-17 19:08:58 -0400

    Update bess-opt.py

--- bess-opt.py
+++ bess-opt.py
@@ -11,6 +11,13 @@ import pandas as pd
 #width
 st.set_page_config(layout="wide")
 
+#hide menu bars
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
 
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
