a6e38c5ccae4729b0d7412756280a21faf6598c4
Duncan Campbell <duncansandersoncampbell@gmail.com>
2023-08-11 14:56:51 -0400

    Update bess-opt.py

--- bess-opt.py
+++ bess-opt.py
@@ -45,13 +45,12 @@ def fetch_solar_output(api_key, address, system_capacity, dc_ac_ratio, module_ty
     # Print the generated API URL for debugging
     generated_url = f"{endpoint}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
     st.write(f"Generated API URL: {generated_url}")
-
+ 
     response = requests.get(endpoint, params=params)
 
     # Print debugging information
     st.write("Status Code:", response.status_code)
     st.write("Headers:", response.headers)
-    st.write("Response Content:", response.text)
     
     if response.status_code != 200:
         st.error(f"API request failed with status code {response.status_code}: {response.text}")
